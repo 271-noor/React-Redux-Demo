f4de3f12486a8503cc1f47344aa8c20385766ac2
271-noor <157873714+271-noor@users.noreply.github.com>
2024-04-05 11:26:10 +0530

    Modified

--- react-redux-demo/src/redux/store.jsx
+++ react-redux-demo/src/redux/store.jsx
@@ -1,6 +1,9 @@
-import { createStore } from 'redux'
-import cakeReducer from './cake/cakeReducer'
+import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+// import { composeWithDevTools } from 'redux-devtools-extention'
+import logger from 'redux-logger'
+import rootReducer from './rootReducer'
 
-const store = createStore(cakeReducer)
+const store = createStore(rootReducer, applyMiddleware(logger, thunk))
 
 export default store
